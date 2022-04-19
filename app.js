cf52e3852728cbd214410e96e36ee430c6c6c1a6
Lenny Thach <lennythach1@gmail.com>
2022-04-19 16:22:56 -0700

    adding bootstrap

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // Register our view engine and middlewares
 app.use(cookieParser());
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
